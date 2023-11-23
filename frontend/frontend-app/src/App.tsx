1ea3a0fe46af3f339e45c53a980f2dbf1c22134b
David Martinez <dmz.mata@gmail.com>
2023-11-22 18:27:24 -0600

    Added homepage comming soon

--- frontend/frontend-app/src/App.tsx
+++ frontend/frontend-app/src/App.tsx
@@ -1,7 +1,16 @@
+import { Homepage } from './pages/Homepage'
+import { Register } from './pages/Register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <>
-      <h1 className='bg-black'>Frontend App</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ frontend/frontend-app/src/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+export const Homepage = () => {
+  return (
+    <>
+      <section className='flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+        <div className='flex h-full w-full items-center justify-center container mx-auto px-8'>
+          <div className='max-w-2xl text-center'>
+            <h1 className='text-3xl sm:text-5xl font-mono capitalize text-white lg:text-7xl'>Comming Soon</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
