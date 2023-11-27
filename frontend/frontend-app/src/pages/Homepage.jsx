b415369e2c2e87bcf1a4ea0f78b5671b700e24f6
David Martinez <dmz.mata@gmail.com>
2023-11-27 15:55:09 -0600

    Quick fix

--- frontend/frontend-app/src/pages/Homepage.jsx
+++ frontend/frontend-app/src/pages/Homepage.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 export const Homepage = () => {
   return (
     <section className='min-h-screen'>
-      <div className='grid grid-cols-2 gap-4'>
-        <div className='h-screen col-auto px-3 py-5'>
+      <div className='grid gap-4 md:grid-cols-2'>
+        <div className='col-auto px-3 py-5 text-center md:text-left md:h-screen'>
           <Link to='/' className='inline-block'>
             <Logo />
           </Link>
         </div>
-        <div className='h-screen col-auto px-3 py-5'>
+        <div className='col-auto px-3 py-5 md:h-screen'>
           <code className='block py-5 text-center'>{'<Form>'}</code>
           <a href='#' className='btn btn-template-1'>
             Ingresar
