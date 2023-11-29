cdbcf4979f0419a1d8e5b865e11f8de1232c7f29
David moreno <davidjmcxl@gmail.com>
2023-11-29 11:03:13 -0500

    chore:add date to msgDto

--- backend/src/main/java/com/trucking/Security/Dto/MsgDto.java
+++ backend/src/main/java/com/trucking/Security/Dto/MsgDto.java
@@ -1,19 +1,26 @@
 package com.trucking.Security.Dto;
 
 
+import java.time.LocalDate;
+
 public class MsgDto {
 
 
 
-    private String mensaje;
-
+    private String message;
+    private LocalDate dateupdate ;
 
 
     public MsgDto(String msg) {
-        this.mensaje= msg;
+
+        this.message= msg;
+        this.dateupdate = LocalDate.now();
     }
 
     public  String getMessage(){
-        return this.mensaje;
+        return this.message;
+    }
+    public LocalDate getApplicationDate() {
+        return this.dateupdate;
     }
 }
