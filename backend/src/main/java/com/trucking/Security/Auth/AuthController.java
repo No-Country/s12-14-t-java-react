8675f2c0b31cde61625f5d120bf259c61e350991
Luis Peche <71164676+Rpeche-pk@users.noreply.github.com>
2023-11-29 22:52:45 -0500

    Update AuthController.java

--- backend/src/main/java/com/trucking/Security/Auth/AuthController.java
+++ backend/src/main/java/com/trucking/Security/Auth/AuthController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-@SecurityRequirement(name = "bearerAuth")
+//@SecurityRequirement(name = "bearerAuth")
 @Tag(name = "Management Auth User", description = "Client authentication and registration management")
 public class AuthController {
 
