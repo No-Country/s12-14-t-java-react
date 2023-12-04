aec3cc9bc27113915f538cfcbbcacfa759bcf28a
David Martinez <dmz.mata@gmail.com>
2023-12-04 14:02:40 -0600

    Update dashboard

---
+++ frontend/frontend-app/src/components/SidebarEmpleado/SidebarEmpleado.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const SidebarEmpleado = () => {
+  return <div>SidebarEmpleado</div>
+}
---
+++ frontend/frontend-app/src/pages/DashboardGerente.jsx
@@ -0,0 +1,14 @@
+import { Sidebar } from '../components/Sidebar/Sidebar'
+import { SidebarEmpleado } from '../components/Sidebar/SidebarEmpleado'
+
+export const DashboardGerente = () => {
+  return (
+    <>
+      <Sidebar></Sidebar>
+
+      
+
+      <SidebarEmpleado></SidebarEmpleado>
+    </>
+  )
+}
