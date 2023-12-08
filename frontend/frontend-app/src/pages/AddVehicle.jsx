f484255fc47fb3ed60aef8efbc899d598da1662f
cricritoia <toiacristiandaniel@gmail.com>
2023-12-08 14:10:53 -0300

    actualizando responsive

--- frontend/frontend-app/src/pages/AddVehicle.jsx
+++ frontend/frontend-app/src/pages/AddVehicle.jsx
@@ -7,52 +7,52 @@ const AddVehicle = () => {
 
     return (
 
-        <div className="w-80 h-full my-4">
+        <div className="w-[95%] h-full my-10%">
             
-            <h1 className="ml-6 mt-16 text-[#0D1444] text-[22px] font-bold font-['Poppins']">
+            <h1 className="ml-[10%] mt-16 text-[#0D1444] text-[22px] font-bold font-['Poppins']">
             Agregar vehículo
             </h1>
 
-            <h2 className="ml-6 my-4 text-[#0D1444] text-lg font-medium font-['Poppins'] 
+            <h2 className="ml-[10%] my-[5%] text-[#0D1444] text-lg font-medium font-['Poppins'] 
             leading-normal tracking-tight">
             Tipo de vehículo
             </h2>
         
             <div>
 
-                <section className="ml-6 mt-2">
+                <section className="ml-[10%] mt-[2.5%]">
                 
-                    <div className="my-2">
+                    <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option1">
                             <input  className="mr-2 h-4 w-4 appearance-none rounded-full border-2 border-[#0D1444] checked:bg-[#0D1444] checked:border-transparent"  type="radio" id="opcion1" name="vehicleType" value="option1" />
                             <span className="inline-block align-middle">Acoplado</span>
                         </label>
                     </div>
-                    <div className="my-2">
+                    <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option2">
                             <input className="mr-2 h-4 w-4 appearance-none rounded-full border-2 border-[#0D1444] checked:bg-[#0D1444] checked:border-transparent" type="radio" id="option2" name="vehicleType" value="option2" />
                             <span className="inline-block align-middle">Automóvil</span>
                         </label>
                     </div>
-                    <div className="my-2">
+                    <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option3">
                             <input className="mr-2 h-4 w-4 appearance-none rounded-full border-2 border-[#0D1444] checked:bg-[#0D1444] checked:border-transparent" type="radio" id="option3" name="vehicleType" value="option3" />
                             <span className="inline-block align-middle">Camión</span>
                         </label>
                     </div>
-                    <div className="my-2">
+                    <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option4">
                             <input className="mr-2 h-4 w-4 appearance-none rounded-full border-2 border-[#0D1444] checked:bg-[#0D1444] checked:border-transparent" type="radio" id="option4" name="vehicleType" value="option4" />
                             <span className="inline-block align-middle">Camioneta</span>
                         </label>
                     </div>
-                    <div className="my-2">
+                    <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option5">
                             <input className="mr-2 h-4 w-4 appearance-none rounded-full border-2 border-[#0D1444] checked:bg-[#0D1444] checked:border-transparent" type="radio" id="option5" name="vehicleType" value="option5" />
                             <span className="inline-block align-middle">Combi</span>
                         </label>
                     </div>
-                    <div className="my-2">
+                    <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option6">
                             <input className="mr-2 h-4 w-4 appearance-none rounded-full border-2 border-[#0D1444] checked:bg-[#0D1444] checked:border-transparent" type="radio" id="option6" name="vehicleType" value="option6" />
                             <span className="inline-block align-middle">Semiremolque</span>
@@ -61,35 +61,35 @@ const AddVehicle = () => {
 
                 </section>
 
-                <section className="mt-4">
+                <section className="mt-[5%]">
 
-                    <div className="relative mb-4">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 top-0 left-6 text-[#0D1444] text-sm">
+                    <div className="relative mb-[5%]">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
                         Marca
                         </label>
                         <input type="text" 
                         id="inputField" 
                         name="inputField" 
-                        className="border border-[#0D1444] py-2 px-4 w-5/6 h-14 mx-4 my-3 rounded"/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-[2.5%] rounded"/>
                     </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 top-0 left-6 text-[#0D1444] text-sm">
+                    <div className="relative mb-[5%]">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
                         Modelo
                         </label>
                         <input type="text" 
                         id="inputField" 
                         name="inputField" 
-                        className="border border-[#0D1444] py-2 px-4 w-5/6 h-14 mx-4 my-3 rounded"/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-[2.5%]  rounded"/>
                     </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 top-0 left-6 z-10 text-[#0D1444] text-sm">
+                    <div className="relative mb-[5%]">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] z-10 text-[#0D1444] text-sm">
                         Año
                         </label>
                         <div className="relative">
                         <input type="text" 
                         id="yearField" 
                         name="yearField" 
-                        className="border border-[#0D1444] py-2 px-4 w-5/6 h-14 mx-4 my-3 rounded "
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-[2.5%]  rounded "
                         placeholder="aaaa"
                         />
                         <select id="yearSelect" 
@@ -128,45 +128,46 @@ const AddVehicle = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 top-0 left-6 text-sm">
+                    <div className="relative mb-[5%]">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-sm">
                         Patente
                         </label>
                         <input type="text" 
                         id="inputField" 
                         name="inputField" 
-                        className="border border-[#0D1444] py-2 px-4 w-5/6 h-14 mx-4 my-3 rounded"/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-[2.5%]  rounded"/>
                     </div>
                 </section>
             </div>
 
-            <h2 className="ml-6 my-4 text-[#0D1444] text-lg font-medium font-['Poppins'] 
+            <h2 className="ml-[10%] mt-[10%] mb-[5%] text-[#0D1444] text-lg font-medium font-['Poppins'] 
             leading-normal tracking-tight">
             Tipo de combustible
             </h2>
-            <div className="">
 
-                <section className="ml-6 mt-2">
+            <div>
+
+                <section className="ml-[10%] mt-[0%]">
                 
-                    <div className="my-2">
+                    <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option1">
                             <input  className="mr-2 h-4 w-4 appearance-none rounded-full border-2 border-[#0D1444] checked:bg-[#0D1444] checked:border-transparent"  type="radio" id="opcion1" name="vehicleType" value="option1" />
                             <span className="inline-block align-middle">Biocombustible</span>
                         </label>
                     </div>
-                    <div className="my-2">
+                    <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option2">
                             <input className="mr-2 h-4 w-4 appearance-none rounded-full border-2 border-[#0D1444] checked:bg-[#0D1444] checked:border-transparent" type="radio" id="option2" name="vehicleType" value="option2" />
                             <span className="inline-block align-middle">Gas</span>
                         </label>
                     </div>
-                    <div className="my-2">
+                    <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option3">
                             <input className="mr-2 h-4 w-4 appearance-none rounded-full border-2 border-[#0D1444] checked:bg-[#0D1444] checked:border-transparent" type="radio" id="option3" name="vehicleType" value="option3" />
                             <span className="inline-block align-middle">Gasoil</span>
                         </label>
                     </div>
-                    <div className="my-2">
+                    <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option4">
                             <input className="mr-2 h-4 w-4 appearance-none rounded-full border-2 border-[#0D1444] checked:bg-[#0D1444] checked:border-transparent" type="radio" id="option4" name="vehicleType" value="option4" />
                             <span className="inline-block align-middle">Nafta</span>
@@ -175,98 +176,98 @@ const AddVehicle = () => {
 
                 </section>
 
-                <section className="mt-4">
+                <section className="mt-[5%]">
 
-                    <div className="relative mb-4">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 top-0 left-6 text-[#0D1444] text-sm">
+                    <div className="relative mb-[5%]">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
                         Marca de motor
                         </label>
                         <input type="text" 
                         id="inputField" 
                         name="inputField" 
-                        className="border border-[#0D1444] py-2 px-4 w-5/6 h-14 mx-4 my-3 rounded"/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-[2.5%] rounded"/>
                     </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 top-0 left-6 text-[#0D1444] text-sm">
+                    <div className="relative mb-[5%]">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
                         N° de motor
                         </label>
                         <input type="text" 
                         id="inputField" 
                         name="inputField" 
-                        className="border border-[#0D1444] py-2 px-4 w-5/6 h-14 mx-4 my-3 rounded"/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-[2.5%] rounded"/>
                     </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 top-0 left-6 z-10 text-[#0D1444] text-sm">
+                    <div className="relative mb-[5%]">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] z-10 text-[#0D1444] text-sm">
                         Marca de chasis
                         </label>
                         <div className="relative">
                         <input type="text" 
                         id="yearField" 
                         name="yearField" 
-                        className="border border-[#0D1444] py-2 px-4 w-5/6 h-14 mx-4 my-3 rounded "/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-[2.5%] rounded "/>
                         </div>
                     </div>
                     
-                    <div className="relative mb-4">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 top-0 left-6 text-[#0D1444] text-sm">
+                    <div className="relative mb-[5%]">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
                         Cantidad de ejes
                         </label>
                         <input type="text" 
                         id="inputField" 
                         name="inputField" 
-                        className="border border-[#0D1444] py-2 px-4 w-5/6 h-14 mx-4 my-3 rounded"/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-[2.5%] rounded"/>
                     </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 top-0 left-6 text-[#0D1444] text-sm">
+                    <div className="relative mb-[5%]">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
                         Fecha de Vtv
                         </label>
                         <input type="text" 
                         id="inputField" 
                         name="inputField" 
-                        className="border border-[#0D1444] py-2 px-4 w-5/6 h-14 mx-4 my-3 rounded"/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-[2.5%] rounded"/>
                     </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 top-0 left-6 text-[#0D1444] text-sm">
+                    <div className="relative mb-[5%]">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
                         Cantidad de ejes
                         </label>
                         <input type="text" 
                         id="inputField" 
                         name="inputField" 
-                        className="border border-[#0D1444] py-2 px-4 w-5/6 h-14 mx-4 my-3 rounded mb-4"/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-[2.5%] rounded mb-4"/>
                     </div>
                 </section>
             </div>
 
-            <div className="bg-[#31429B] w-80 h-80">
+            <div className="bg-[#31429B] w-[100%] mx-[2.5%] lg:mx-[3.5%]">
             <h3 className="text-white w-4/5 text-start py-4 mx-4 font-semibold">REGISTRO ÚNICO DE TRANSPORTE DEL AUTOMOTOR</h3>
             <div className="relative my-0">
-                        <label htmlFor="inputField" className="absolute bg-[#31429B] px-1 top-0 left-6 z-10 text-white text-sm">
+                        <label htmlFor="inputField" className="absolute bg-[#31429B] px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] z-10 text-white text-sm">
                         Constancia de inscripción
                         </label>
                         <div className="relative">
                         <input type="text" 
                         id="yearField" 
                         name="yearField" 
-                        className="border bg-[#31429B] border-white py-2 px-4 h-14 w-5/6 mx-4 my-3 rounded "/>
+                        className="border bg-[#31429B] border-white py-2 px-4 h-14 w-[85%] mx-[10%] my-[2.5%] rounded "/>
                         </div>
                     </div>
                     
                     <div className="relative mb-2">
-                        <label htmlFor="inputField" className="absolute bg-[#31429B] px-1 top-0 left-6 text-white text-sm">
+                        <label htmlFor="inputField" className="absolute bg-[#31429B] px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-white text-sm">
                         Categoría
                         </label>
                         <input type="text" 
                         id="inputField" 
                         name="inputField" 
-                        className="border bg-[#31429B] border-white py-2 px-4 h-14 w-5/6 mx-4 my-3 rounded"/>
+                        className="border bg-[#31429B] border-white py-2 px-4 h-14 w-[85%] mx-[10%] my-[2.5%] rounded"/>
                     </div>
-            <button className="w-2/3 h-8 ml-4 mt-2 rounded-sm bg-white text-[#0D1444] font-semibold text-xs">
+            <button className="w-2/3 h-8 mx-[10%] my-[5%] rounded-md bg-white text-[#0D1444] font-semibold text-xs">
             seleccionar constancia
             </button>
 
             </div>
-            <div className="w-80 h-20">
-                <button className="w-3/4 h-1/2 bg-[#31429B] mx-7 my-5 rounded-md text-white">
+            <div className="w-[95%] h-20">
+                <button className="w-[80%] h-1/2 bg-[#31429B] mx-[15%] my-[12%] rounded-md text-white">
                 Agregar vehículo
                 </button>
             </div>
