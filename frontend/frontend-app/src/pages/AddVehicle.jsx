ad86986d19ef82a9335825686d37f7a5d7119c43
cricritoia <toiacristiandaniel@gmail.com>
2023-12-09 19:38:12 -0300

    iniciando funcionalidades

--- frontend/frontend-app/src/pages/AddVehicle.jsx
+++ frontend/frontend-app/src/pages/AddVehicle.jsx
@@ -1,13 +1,13 @@
+import {SelectAño, selectedYear} from "../components/SelectAño";
+
+
 
 const AddVehicle = () => {
-
-    
-
     
 
     return (
 
-        <div className="w-[95%] h-full md:h-[90%] my-10%  md:w-[50%] md:ml-[35%]">
+        <div className="w-[95%] h-full md:h-[90%] my-10%  md:w-[50%] md:ml-[35%] lg:w-[60%] lg:ml-[16%]">
             
             <h1 className="ml-[10%] mt-16 text-[#0D1444] text-[22px] font-bold font-['Poppins']">
             Agregar vehículo
@@ -20,7 +20,7 @@ const AddVehicle = () => {
         
             <div>
 
-                <section className="ml-[10%] mt-[2.5%]">
+                <section className="ml-[10%] mt-[2.5%] lg:grid lg:grid-cols-6">
                 
                     <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option1">
@@ -61,10 +61,10 @@ const AddVehicle = () => {
 
                 </section>
 
-                <section className="mt-[5%]">
+                <section className="mt-[5%] lg:grid lg:grid-cols-2 lg:ml-[8%]">
 
                     <div className="relative mb-[5%]">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[8%] left-[12%] text-[#0D1444] text-sm">
                         Marca
                         </label>
                         <input type="text" 
@@ -73,7 +73,7 @@ const AddVehicle = () => {
                         className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4 rounded"/>
                     </div>
                     <div className="relative mb-[5%]">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[8%] left-[12%] text-[#0D1444] text-sm">
                         Modelo
                         </label>
                         <input type="text" 
@@ -82,54 +82,29 @@ const AddVehicle = () => {
                         className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4  rounded"/>
                     </div>
                     <div className="relative mb-[5%]">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] z-10 text-[#0D1444] text-sm">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[8%] left-[12%] z-10 text-[#0D1444] text-sm">
                         Año
                         </label>
                         <div className="relative">
                         <input type="text" 
                         id="yearField" 
-                        name="yearField" 
-                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4  rounded "
+                        name="yearField"
+                        value={selectedYear}
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4  rounded"
                         placeholder="aaaa"
                         />
                         <select id="yearSelect" 
-                        name="yearSelect" 
-                        className=" bg-blue absolute inset-0 opacity-0 cursor-pointer w-1/2 h-1/2 left-20 z-10">
-                                <option value="" disabled selected hidden>Seleccione el año</option>
-                                <option value="2023">2023</option>
-                                <option value="2022">2022</option>
-                                <option value="2021">2021</option>
-                                <option value="2020">2020</option>
-                                <option value="2019">2019</option>
-                                <option value="2018">2018</option>
-                                <option value="2017">2017</option>
-                                <option value="2016">2016</option>
-                                <option value="2015">2015</option>
-                                <option value="2014">2014</option>
-                                <option value="2013">2013</option>
-                                <option value="2012">2012</option>
-                                <option value="2011">2011</option>
-                                <option value="2010">2010</option>
-                                <option value="2009">2009</option>
-                                <option value="2008">2008</option>
-                                <option value="2007">2007</option>
-                                <option value="2006">2006</option>
-                                <option value="2005">2005</option>
-                                <option value="2004">2004</option>
-                                <option value="2003">2003</option>
-                                <option value="2002">2002</option>
-                                <option value="2001">2001</option>
-                                <option value="2000">2000</option>
-                               
-                            </select>
-                       
-                            <div className="pointer-events-none absolute inset-y-0 right-0 flex 
+                            name="yearSelect" 
+                            className=" bg-blue absolute inset-0 opacity-0 cursor-pointer w-1/2 h-1/2 left-20 z-10">
+                            <SelectAño/>                              
+                        </select>
+                        <div className="pointer-events-none absolute inset-y-0 right-0 flex 
                             items-center px-2 text-gray-700">
                             </div>
                         </div>
                     </div>
                     <div className="relative mb-[5%]">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-sm">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[8%] left-[12%] text-sm">
                         Patente
                         </label>
                         <input type="text" 
@@ -147,7 +122,7 @@ const AddVehicle = () => {
 
             <div>
 
-                <section className="ml-[10%] mt-[0%]">
+                <section className="ml-[10%] mt-[0%] lg:grid lg:grid-cols-4 lg:ml-[15%]">
                 
                     <div className="my-[2.5%]">
                         <label className="text-[#0D1444] text-sm flex items-center" htmlFor="option1">
@@ -176,10 +151,10 @@ const AddVehicle = () => {
 
                 </section>
 
-                <section className="mt-[5%]">
+                <section className="mt-[5%] lg:grid lg:grid-cols-2 lg:ml-[8%]">
 
                     <div className="relative mb-[5%]">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%]  left-[12%] text-[#0D1444] text-sm">
                         Marca de motor
                         </label>
                         <input type="text" 
@@ -188,7 +163,7 @@ const AddVehicle = () => {
                         className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4 rounded"/>
                     </div>
                     <div className="relative mb-[5%]">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] left-[12%] text-[#0D1444] text-sm">
                         N° de motor
                         </label>
                         <input type="text" 
@@ -197,20 +172,20 @@ const AddVehicle = () => {
                         className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4 rounded"/>
                     </div>
                     <div className="relative mb-[5%]">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] z-10 text-[#0D1444] text-sm">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] left-[12%] z-10 text-[#0D1444] text-sm">
                         Marca de chasis
                         </label>
                         <div className="relative">
                         <input type="text" 
                         id="yearField" 
                         name="yearField" 
-                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4 rounded "/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4 rounded"/>
                         </div>
                     </div>
                     
                     <div className="relative mb-[5%]">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
-                        Cantidad de ejes
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] left-[12%] text-[#0D1444] text-sm">
+                        N° de chasis
                         </label>
                         <input type="text" 
                         id="inputField" 
@@ -218,7 +193,7 @@ const AddVehicle = () => {
                         className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4 rounded"/>
                     </div>
                     <div className="relative mb-[5%]">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] left-[12%] text-[#0D1444] text-sm">
                         Fecha de Vtv
                         </label>
                         <input type="text" 
@@ -227,20 +202,31 @@ const AddVehicle = () => {
                         className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4 rounded"/>
                     </div>
                     <div className="relative mb-[5%]">
-                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%] lg:top-[19%] left-[12%] text-[#0D1444] text-sm">
+                        <label htmlFor="inputField" className="absolute bg-white px-1 -top-[5%] md:top-[8%]  left-[12%] text-[#0D1444] text-sm">
                         Cantidad de ejes
                         </label>
-                        <input type="text" 
+                        <input type="text"
                         id="inputField" 
                         name="inputField" 
-                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4 rounded mb-4"/>
+                        className="border border-[#0D1444] py-2 px-4 w-[85%] h-14 mx-[10%] my-4 rounded mb-4"                        placeholder="xx"
+                        />
+                        <select id="ejesSelect" 
+                        name="ejesSelect" 
+                        className=" bg-blue absolute inset-0 opacity-0 cursor-pointer w-1/2 h-1/2 left-20 z-10">                        
+                        <option value="1" disabled selected hidden >Seleccione cantidad de ejes</option>
+                        <option value="2">2 ejes</option>
+                        <option value="3">3 ejes</option>
+                        <option value="4">4 ejes</option>
+                        <option value="5">5 ejes</option>
+                        </select>
                     </div>
                 </section>
             </div>
 
-            <div className="bg-[#31429B] w-[100%] mx-[2.5%] md:-mx-[22%] md:w-[150%]">
+            <div className="bg-[#31429B] w-[100%] mx-[2.5%] md:-mx-[22%] md:w-[150%] lg:w-[100%] lg:mx-[5%]">
             <h3 className="text-white w-4/5 text-start py-4 mx-4 font-semibold">REGISTRO ÚNICO DE TRANSPORTE DEL AUTOMOTOR</h3>
-            <div className="relative my-0">
+                <div className="lg:grid lg:grid-cols-3">
+                    <div className="relative my-0">
                         <label htmlFor="inputField" className="absolute bg-[#31429B] px-1 -top-[5%]  left-[12%] z-10 text-white text-sm md:left-[22%] lg:top-[8%]">
                         Constancia de inscripción
                         </label>
@@ -248,7 +234,7 @@ const AddVehicle = () => {
                         <input type="text" 
                         id="yearField" 
                         name="yearField" 
-                        className="border bg-[#31429B] border-white py-2 px-4 h-14 w-[85%] mx-[8%] my-4 rounded md:w-[57%] md:mx-[21%]"/>
+                        className="border bg-[#31429B] border-white py-2 px-4 h-14 w-[85%] mx-[8%] my-4 rounded md:w-[57%] md:mx-[21%] lg:w-[70%] lg:ml-[22%]"/>
                         </div>
                     </div>
                     
@@ -259,15 +245,28 @@ const AddVehicle = () => {
                         <input type="text" 
                         id="inputField" 
                         name="inputField" 
-                        className="border bg-[#31429B] border-white py-2 px-4 h-14 w-[85%] mx-[8%] my-4 rounded md:w-[57%] md:mx-[21%]"/>
+                        className="border bg-[#31429B] border-white py-2 px-4 h-14 w-[85%] mx-[8%] my-4 rounded md:w-[57%] md:mx-[21%] lg:w-[70%] lg:ml-[22%]"/>
+                         <select id="ejesSelect" 
+                        name="ejesSelect" 
+                        className=" bg-blue absolute inset-0 opacity-0 cursor-pointer w-1/2 h-1/2 left-20 z-10">                        
+                        <option value="1" disabled selected hidden >Seleccione la categoría</option>
+                        <option value="2">Categoría 1</option>
+                        <option value="3">Categoría 2</option>
+                        <option value="4">Categoría 3</option>
+                        <option value="5">Categoría 4</option>
+                        </select>
                     </div>
-            <button className="w-[65%] h-8 mx-[8%] mb-4 rounded-md bg-white text-[#0D1444] font-semibold text-xs md:w-[30%] md:mx-[21%]">
-            seleccionar constancia
-            </button>
+                    <div>
+                    <button className="w-[65%] h-8 mx-[8%] mb-0 rounded-md bg-white text-[#0D1444] font-semibold text-xs md:w-[30%] md:mx-[21%] lg:mt-[5%] lg:w-[55%]">
+                    seleccionar constancia
+                    </button>
+                    <p className="text-white mx-[10%] pb-4 md:mx-[22%]">imagen.jpg</p>
+                    </div>
+                </div>
 
             </div>
             <div className="w-[95%] h-20">
-                <button className="w-[90%] h-8 bg-[#31429B] mx-[10%] my-[12%] rounded-md text-white md:w-[90%]">
+                <button className="w-[90%] h-8 bg-[#31429B] mx-[10%] my-[12%] rounded-md text-white md:w-[90%] lg:mx-[12%]">
                 Agregar vehículo
                 </button>
             </div>
