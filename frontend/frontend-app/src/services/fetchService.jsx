2896a47f69399a7151df55ec24666d03edec123e
jebius76 <91204905+jebius76@users.noreply.github.com>
2023-12-16 12:18:04 -0300

    feat(empleados): Conexion con enpoint para listar empleados.

--- frontend/frontend-app/src/services/fetchService.jsx
+++ frontend/frontend-app/src/services/fetchService.jsx
@@ -5,6 +5,10 @@ export const getActiveVehicles = () => {
   return pageApi.get('http://149.50.131.109:9896/api/v1/list/vehicle/getAllActive?page=0&size=10');
 } 
 
+export const getEmployees = () => {
+  return pageApi.get('http://149.50.131.109:9896/api/v1/employee/allEmployees');
+} 
+
 export const postVehicle = (data) => {
   console.log("Token : " + localStorage.getItem('token'))
   console.log("Vehicle: ")
@@ -19,3 +23,4 @@ export const postEmployee = (data) => {
   console.log(data)
   return pageApi.post(`http://149.50.131.109:9896/api/v1/employee/newEmployee`, data)
 } 
+
