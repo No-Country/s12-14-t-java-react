f5223d19577818d6ea4640669ce2afdfe7744fb3
jebius76 <91204905+jebius76@users.noreply.github.com>
2023-12-16 11:14:24 -0300

    feat(conections): Added messages to form submit.

--- frontend/frontend-app/src/services/fetchService.jsx
+++ frontend/frontend-app/src/services/fetchService.jsx
@@ -1,50 +1,21 @@
 import axios from "axios";
 import { pageApi } from "../api/PageApi";
-export const getAllClients = async (token) => {
-
-  console.log("Token:"+token)
-  let response = await fetch('http://149.50.131.109:9896/api/v1/list/vehicle/getAllActive?page=0&size=10', {
-      method: 'GET',
-//        mode: 'no-cors',
-//        credentials: "omit",
-      cache: 'no-cache',
-      headers: {
-      'Content-type': 'application/json',
-      'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-
-  });
-  return response.json();
-}
 
 export const getActiveVehicles = () => {
-
-  return axios({
-        method: 'get',
-        url: `http://149.50.131.109:9896/api/v1/list/vehicle/getAllActive?page=0&size=10`,
-        withCredentials: false,
-        headers: {
-          'Authorization': 'Bearer ' + localStorage.getItem('token'),
-
-        },
-      });
+  return pageApi.get('http://149.50.131.109:9896/api/v1/list/vehicle/getAllActive?page=0&size=10');
 } 
 
 export const postVehicle = (data) => {
-
   console.log("Token : " + localStorage.getItem('token'))
   console.log("Vehicle: ")
   console.log(data)
-
   return pageApi.post(`http://149.50.131.109:9896/api/v1/vehicle/save`, data)
 } 
 
 export const postEmployee = (data) => {
-
   data.photo = photo.value
   console.log("Token : " + localStorage.getItem('token'))
   console.log("Employee: ")
   console.log(data)
-
   return pageApi.post(`http://149.50.131.109:9896/api/v1/employee/newEmployee`, data)
 } 
