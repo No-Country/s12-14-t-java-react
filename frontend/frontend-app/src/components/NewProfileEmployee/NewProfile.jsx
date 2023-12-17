4a66650becd29650a64c3835ecc0c59f46c08b23
CarlosV319 <carlos.alberto_88@hotmail.com>
2023-12-16 23:05:46 -0300

    limpiando código

--- frontend/frontend-app/src/components/NewProfileEmployee/NewProfile.jsx
+++ frontend/frontend-app/src/components/NewProfileEmployee/NewProfile.jsx
@@ -185,7 +185,7 @@ const NewProfile = () => {
                     id="photo"
                     type="file"
                     className="block w-full text-sm file:mr-4 file:rounded-md file:border-0 file:bg-dark-blue-500 file:py-2.5 file:px-4 file:text-sm file:font-semibold file:text-white hover:file:bg-dark-blue-700 focus:outline-none disabled:pointer-events-none disabled:opacity-60"
-                    {...register('photo', { required: 'Campo requerido' })}
+               
                   />
                 </div>
 
@@ -200,7 +200,7 @@ const NewProfile = () => {
                       id="rol1"
                       name="roleName"
                       className="h-4 w-4 rounded-full border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400"
-                      {...register('roleName', { required: 'Campo requerido' })}
+                   
                     />
                     <label
                       htmlFor="rol1"
@@ -218,7 +218,7 @@ const NewProfile = () => {
                       id="rol2"
                       name="roleName"
                       className="h-4 w-4 rounded-full border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400"
-                      {...register('roleName', { required: 'Campo requerido' })}
+               
                     />
                     <label
                       htmlFor="rol2"
@@ -236,7 +236,7 @@ const NewProfile = () => {
                       id="rol3"
                       name="roleName"
                       className="h-4 w-4 rounded-full border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400"
-                      {...register('roleName', { required: 'Campo requerido' })}
+                 
                     />
                     <label
                       htmlFor="rol3"
