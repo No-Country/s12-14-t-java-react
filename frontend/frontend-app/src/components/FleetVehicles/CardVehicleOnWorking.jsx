28f99757615f66ed7d3f1e787b9c538d5c585062
David Martinez <dmz.mata@gmail.com>
2023-12-18 10:10:10 -0600

    Merge

--- frontend/frontend-app/src/components/FleetVehicles/CardVehicleOnWorking.jsx
+++ frontend/frontend-app/src/components/FleetVehicles/CardVehicleOnWorking.jsx
@@ -1,69 +1,110 @@
-import deleteIcon from "/img/deleteIcon.svg";
-import editIcon from "/img/editIcon.svg";
-import { useEffect, useState } from "react";
-import {v4 as uuidv4} from "uuid";
-const CardVehicleOnWorking = (props) => {
-  const deleteVehicle = () => {};
-  const editVehicle = () => {};
+import deleteIcon from '/img/deleteIcon.svg'
+import editIcon from '/img/editIcon.svg'
+import { useEffect, useState } from 'react'
+import { deleteVehicle } from '../../services/fetchService'
+import { v4 as uuidv4 } from 'uuid'
+import Swal from 'sweetalert2'
+const CardVehicleOnWorking = props => {
+  const editVehicle = () => {}
+
+  const handleDelete = async e => {
+    e.preventDefault()
+
+    Swal.fire({
+      title: '¿Estás seguro de que quieres eliminar el vehiculo?',
+      text: '¡No podrás revertir esto!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#31429B',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '¡Sí, bórralo!',
+      cancelButtonText: 'Cancelar'
+    }).then(result => {
+      if (result.isConfirmed) {
+        // console.log(props.vehicle.id)
+        // const response = deleteVehicle({ id: props.vehicle.id })
+        deleteVehicle({ id: props.vehicle.id })
+          .then(response => {
+            console.log(response)
+            Swal.fire({
+              title: '¡Eliminado!',
+              text: 'Su vehiculo ha sido eliminado.',
+              icon: 'success'
+            })
+          })
+          .catch(err => {
+            console.log(err)
+            Swal.fire(`Error al eliminar el vehiculo!`)
+          })
+      }
+    })
+  }
+
   const iconDelete = (
     <img
       src={deleteIcon}
-      alt="Icono para eliminar"
-      className="w-[20px] h-5 cursor-pointer hover:bg-[#9299b5]"
+      alt='Icono para eliminar'
+      className='border-b-2 pb-[2px] cursor-pointer border-[#31429B] group-hover:border-white'
       onClick={deleteVehicle}
     />
-  );
-  const iconEdit = (
-    <img
-      src={editIcon}
-      alt="Icono de editar"
-      className="w-[20px] h-5 cursor-pointer hover:bg-[#9299b5] "
-      onClick={editVehicle}
-    />
-  );
-  const [isChecked, setIsChecked] = useState(false);
-  const idCheckBox=uuidv4();
+  )
+  // const iconEdit = (
+  //   <img
+  //     src={editIcon}
+  //     alt='Icono de editar'
+  //     className='p-3 cursor-pointer hover:bg-[#9299b5] '
+  //     onClick={editVehicle}
+  //   />
+  // )
+  const [isChecked, setIsChecked] = useState(false)
+  const idCheckBox = uuidv4()
   const handleCheckBoxChange = () => {
-    setIsChecked(!isChecked);
-  };
+    setIsChecked(!isChecked)
+  }
 
   return (
-    <article className="card">
-      <header className="bg-[#31429B] w-full h-9 flex flex-row justify-around  items-center rounded-t-[10px] ">
-        <h2 className="text-[#fff] text-[14px] font-semibold">
-          {props.vehicle.id}
-        </h2>
-        <div className="flex flex-row gap-1">
-          {iconEdit}
-          {iconDelete}
+    <article className='card'>
+      <header className='bg-[#31429B] w-full flex flex-row justify-between items-center rounded-t-lg pl-3'>
+        <p className='text-[#fff] text-[16px] font-semibold'>{props.vehicle.id}</p>
+        <div className='flex flex-row gap-1'>
+          {/* <a href='#'>{iconEdit}</a> */}
+          <a href='#' className='px-3 pt-3 pb-2 group border-color' onClick={handleDelete}>
+            {iconDelete}
+          </a>
         </div>
       </header>
-      <div className="w-full flex flex-row justify-around items-start bg-[#F0F8FF]">
-        <div className="text-[13px] font-bold leading-6 text-[#0D1544] ">
+      <div className='w-full flex flex-row items-start justify-between bg-[#F0F8FF] px-3 py-2'>
+        <div className='text-[13px] font-bold leading-6 text-[#0D1544] '>
           <p>{props.vehicle.vehicleType}</p>
           <p>{props.vehicle.model}</p>
           <p>{props.vehicle.patent}</p>
           <p>{props.vehicle.brand}</p>
         </div>
-        <span className=" text-[#0D1544] font-bold leading-6">{props.vehicle.year}</span>
+        <span className=' text-[#0D1544] font-bold leading-6'>{props.vehicle.year}</span>
       </div>
-      <footer className="bg-[#31429B] w-full h-9 flex flex-row justify-center items-center rounded-b-[10px]">
+      <footer className='bg-[#31429B] w-full py-2 flex flex-row items-center rounded-b-lg px-3'>
         <div>
-            <input className="custom-checkbox-out"
-              type="checkbox"
+          <label htmlFor={idCheckBox} className='flex align-baseline cursor-pointer'>
+            <input
+              className='custom-checkbox-out'
+              type='checkbox'
               id={idCheckBox}
               checked={isChecked}
               onChange={handleCheckBoxChange}
             />
-          <div className={`new-checkbox `+ (isChecked? 'checked':'not-checked')}
-          onClick={handleCheckBoxChange}
-          >&#10003;</div>
-          <label className="text-[#fff] text-[14px] font-normal leading-5 tracking-[0.25px] " htmlFor={idCheckBox}>
-            Fuera de servicio
+            <div
+              className={`new-checkbox ` + (isChecked ? 'checked' : 'not-checked')}
+              onClick={handleCheckBoxChange}
+            >
+              &#10003;
+            </div>
+            <span className='text-[#fff] text-[14px] font-normal leading-5 tracking-[0.25px] '>
+              Fuera de servicio
+            </span>
           </label>
         </div>
       </footer>
     </article>
-  );
-};
-export default CardVehicleOnWorking;
+  )
+}
+export default CardVehicleOnWorking
--- frontend/frontend-app/src/services/fetchService.jsx
+++ frontend/frontend-app/src/services/fetchService.jsx
@@ -1,24 +1,27 @@
-
-import { pageApi } from "../api/PageApi";
+import { pageApi } from '../api/PageApi'
 
 // export const getActiveVehicles = (size) => {
 //   return pageApi.get(`${import.meta.env.VITE_API_URL}/list/vehicle/getAllActive?page=0&size=${size}`);
-// } 
+// }
 
 export const getInactiveVehicles = () => {
-  return pageApi.get(`${import.meta.env.VITE_API_URL}/list/vehicle/getAllInactive?page=0&size=10`);
-} 
+  return pageApi.get(`${import.meta.env.VITE_API_URL}/list/vehicle/getAllInactive?page=0&size=10`)
+}
 
 export const getEmployees = () => {
-  return pageApi.get('/employee/allEmployees');
-} 
+  return pageApi.get('/employee/allEmployees')
+}
 
-export const postVehicle = (data) => {
-  console.log("Token : " + localStorage.getItem('token'))
-  console.log("Vehicle: ")
+export const deleteVehicle = data => {
+  return pageApi.delete(`https://trucking-jebius.koyeb.app/api/v1/vehicle/${data.id}`)
+}
+
+export const postVehicle = data => {
+  console.log('Token : ' + localStorage.getItem('token'))
+  console.log('Vehicle: ')
   console.log(data)
   return pageApi.post(`${import.meta.env.VITE_API_URL}/vehicle/save`, data)
-} 
+}
 
 // export const postEmployee = (data) => {
 //   data.photo = photo.value
@@ -26,4 +29,4 @@ export const postVehicle = (data) => {
 //   console.log("Employee: ")
 //   console.log(data)
 //   return pageApi.post(`${import.meta.env.VITE_API_URL}/employee/newEmployee`, data)
-// } 
+// }
